0a067946631909e569f83c582f05e7139fe2fad3
kiranpawar1999 <kiranpawar6519@gmail.com>
2026-03-18 22:34:00 +0530

    update backend

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://candid-pixie-df759e.netlify.app"
+}));
 app.use(express.json());
 
 app.get("/", (req, res) =>{
@@ -43,7 +45,8 @@ app.post("/send-email", async (req, res) =>{
     }
 });
 
-app.listen(5000, ()=> {
-    console.log("Server running on http://localhost:5000");
-    
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
